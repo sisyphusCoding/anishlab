7ea61092911642c37b451fda369168976597d50b
Anish Kanna <sautronik@gmail.com>
2022-07-06 17:32:59 +0530

    this

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
-import dynamic from 'next/dynamic'
-
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-     <Layout> 
+  return (
+    <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
-
+  );
 }
 
-export default MyApp
+export default MyApp;
